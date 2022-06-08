6c07fc644de33b35d00a50ff2fe0415ccd8ebffb
Mark Regan <markdregan@gmail.com>
2022-06-08 12:14:42 +0100

    glassnode data_download class updated

--- user_data/litmus/glassnode/download_data.py
+++ user_data/litmus/glassnode/download_data.py
@@ -7,80 +7,74 @@ import pathlib
 
 
 class GlassnodeData:
-    def __init__(self):
+    """Get data from glassnode and store to csv locally"""
+
+    def __init__(self, token: str, timeframe: str, metric_names_file: str = 'metrics_temp.txt'):
         self.API_KEY = '22HCck9cUjuvUTrEvfc50rgcL7v'
-        self.DIR = 'user_data/data/glassnode/'
+        self.DIR = pathlib.Path('user_data/data/glassnode/', timeframe)
+        self.metric_names = self.get_metric_names(filename=metric_names_file)
+        self.token = token
+        self.timeframe = timeframe
 
     def get_metric_names(self, filename: str) -> list:
         """Load names of metrics from external file"""
 
         filepath = (pathlib.Path(__file__).parent / 'data' / filename).resolve()
         with open(filepath) as file:
-            self.metric_names = file.readlines()
-            self.metric_names = [line.rstrip() for line in self.metric_names]
-        return self.metric_names
+            metric_names = file.readlines()
+            metric_names = [line.rstrip() for line in metric_names]
 
-    def get_metric(self, token: str, metric: str, timeframe: str) -> pd.DataFrame:
+        return metric_names
+
+    def get_metric(self, metric: str):
         """Download historical data for a single token and metric."""
 
-        res = requests.get("https://api.glassnode.com/v1/metrics/{}".format(metric),
-                           params={'a': token,
-                                   'i': timeframe,
-                                   'api_key': self.API_KEY})
-        # Check if metric request successful
-        if self.check_request_status(res, token, metric):
-            try:
-                metric_df = pd.read_json(res.text, convert_dates=['t'])
-                metric_df['t'] = pd.to_datetime(metric_df['t'], utc=True)
-                metric_df.rename({'v': metric.replace('/', '_'),
-                                  't': 'date'}, axis='columns', inplace=True)
-                metric_df.set_index('date', inplace=True)
-                return metric_df
-            except ValueError:
-                # valid_requests[token].remove(metric)
-                print(f"Error thrown for {metric} in {token}, skipping to next column.")
+        while True:
+            # Call Glassnode API and get result
+            self.res = requests.get("https://api.glassnode.com/v1/metrics/{}".format(metric),
+                                    params={'a': self.token,
+                                            'i': self.timeframe,
+                                            'api_key': self.API_KEY})
+            # Check if rate limiting kicked in, sleep if so
+            if self.res.status_code == 429:
+                print(f"Status {self.res.status_code}. Pausing for 60 seconds...")
+                time.sleep(60)
 
-    def get_metrics(self, token: str, metrics: list, timeframe: str, save: bool) -> pd.DataFrame:
+            elif self.res.status_code == 200:
+                # Get metric data and save to csv
+                try:
+                    metric_df = pd.read_json(self.res.text, convert_dates=['t'])
+                    metric_df['t'] = pd.to_datetime(metric_df['t'], utc=True)
+                    metric_df.rename({'v': metric.replace('/', '_'),
+                                      't': 'date'}, axis='columns', inplace=True)
+                    metric_df.set_index('date', inplace=True)
+                    return metric_df
+
+                except Exception:
+                    print(f'Error thrown for {metric} for {self.token}: {Exception}')
+                    return None
+            else:
+                print(f'Request error for {metric} for {self.token}: {self.res.text}')
+                return None
+
+    def get_metrics(self):
         """Download data for multiple metrics for a given token."""
 
-        temp_metrics = []
-        while len(metrics) > 0:
+        for metric in self.metric_names:
+
             try:
-                df = self.get_metric(token, metrics[0], timeframe)
+                df = self.get_metric(metric)
                 if df is not None:
-                    temp_metrics.append(df)
-                metrics.pop(0)
+                    metric_name = metric.replace('/', '_')
+                    filename = f'glassnode_{self.token}_{metric_name}_{self.timeframe}.csv'
+                    filepath = pathlib.Path(self.DIR, filename)
+                    df.to_csv(filepath)
+                    print(f'Data saved as {filename} in {self.DIR}')
+
+                elif df is None:
+                    print(f'Empty results returned for {metric}')
+
             except ValueError:
-                print(f"Issue getting {metrics[0]} for {token}")
-                metrics.pop(0)
+                print(f"Issue getting {metric} for {self.token}")
 
-        # Join all metrics together
-        df = pd.concat(temp_metrics, axis=1)
-
-        if save:
-            filename = f'glassnode_{token}_{timeframe}.csv'
-            self.save_data(df, filename)
-            print(f'Data saved as {filename} in {self.DIR}')
-
-        return df
-
-    def save_data(self, df: pd.DataFrame, filename: str):
-        """Save data in glassnode directory"""
-
-        filepath = pathlib.Path(self.DIR, filename)
-        df.to_csv(filepath)
-        return self
-
-    def check_request_status(self, request, token: str, metric: str):
-        """Checks if request is successful type 200 and returns bool"""
-
-        if request.status_code == 200:  # If request was successful / Coin has this metric
-            print(f"Successfully got {metric} metric for {token}")
-            return True
-        elif request.status_code == 429:
-            print(f"Status {request.status_code}. Pausing for 60 seconds...")
-            time.sleep(60)
-            return False
-        else:
-            print(f"Status {request.status_code} Could not get {metric} metric for {token}")
-            return False
+        print('Process complete...')
